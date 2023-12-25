a5a8564031bef5ff97523e2165f0d21acaf6a39f
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-12-26 00:24:59 +0900

    init

--- app.py
+++ app.py
@@ -1,7 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+import pandas as pd
+from cryptography.fernet import Fernet
+import os
+from io import StringIO
+
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('./index.html')
+    return "Flask 서버가 정상적으로 실행되고 있습니다. 검색을 위해 /search 라우트를 사용하세요."
+
+# secret.key 파일로부터 키를 불러오는 함수
+def load_key():
+    key = b'TU0vRuj7uW0T2Gh81dwtC5qTs7djhmqUkzpWRlR06c0='
+    return Fernet(key)
+
+
+# 암호화된 파일을 복호화하여 메모리에 로드하는 함수
+def load_data():
+    fernet = load_key()
+    directory = './main_data/'
+    file_name = 'main_data.csv'
+    encrypted_file_path = os.path.join(directory, file_name)
+
+    if os.path.isfile(encrypted_file_path):
+        with open(encrypted_file_path, 'rb') as encrypted_file:
+            encrypted = encrypted_file.read()
+        decrypted = fernet.decrypt(encrypted)
+        df = pd.read_csv(StringIO(decrypted.decode('utf-8')))
+        return df
+    else:
+        return None
+# 키워드 검색을 수행하는 함수
+def search_keyword_in_columns(df, keyword):
+    search_columns = ['감정평가요약', '특수권리분석', '참고사항']
+    keyword_filter = df[search_columns].apply(lambda x: x.str.contains(keyword, na=False, regex=True)).any(axis=1)
+    result_df = df[keyword_filter]
+    result_columns = ['사건번호', '소재지', '감정가', '토지면적', '건물면적']
+    return result_df[result_columns]
+
+
+# API 루트
+@app.route('/search', methods=['GET'])
+def search():
+    keyword = request.args.get('keyword')
+    if not keyword:
+        return jsonify({'error': 'No keyword provided'}), 400
+
+    df = load_data()
+    if df is not None:
+        result_df = search_keyword_in_columns(df, keyword)
+        return jsonify(result_df.to_dict(orient='records'))
+    else:
+        return jsonify({'error': 'Data not found'}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
