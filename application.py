36866363df53628d6d3b7261500e47f24215db91
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-12-29 02:36:37 +0900

    검색 갯수 추가

--- application.py
+++ application.py
@@ -3,26 +3,24 @@ import pandas as pd
 from cryptography.fernet import Fernet
 import os
 from io import StringIO
-from flask_cors import CORS  # CORS를 위한 라이브러리 임포트
+from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)  # CORS 설정 적용
-app.config['JSON_AS_ASCII'] = False
+application = Flask(__name__)
+CORS(application)
+application.config['JSON_AS_ASCII'] = False
 
-@app.route('/')
+@application.route('/')
 def index():
     return "Flask 서버가 정상적으로 실행되고 있습니다. 검색을 위해 /search 라우트를 사용하세요."
 
-@app.route('/hello_world', methods=['GET'])  # 'hello_world' 라우트와 GET 메소드 추가
+@application.route('/hello_world', methods=['GET'])
 def hello_world():
     return "Hello World"
 
-# secret.key 파일로부터 키를 불러오는 함수
 def load_key():
     key = b'TU0vRuj7uW0T2Gh81dwtC5qTs7djhmqUkzpWRlR06c0='
     return Fernet(key)
 
-# 암호화된 파일을 복호화하여 메모리에 로드하는 함수
 def load_data():
     fernet = load_key()
     directory = './main_data/'
@@ -38,7 +36,6 @@ def load_data():
     else:
         return None
 
-# 키워드 검색을 수행하는 함수
 def search_keyword_in_columns(df, keyword):
     search_columns = ['감정평가요약', '특수권리분석', '참고사항']
     rows = []
@@ -48,20 +45,15 @@ def search_keyword_in_columns(df, keyword):
             text = row[col]
             if pd.isna(text):
                 continue
-            # 키워드가 포함된 문장 찾기
             sentences = text.split('.')
             for sentence in sentences:
                 if keyword in sentence:
                     rows.append({'사건번호': row['사건번호'], '주요키워드': sentence.strip()})
 
     result_df = pd.DataFrame(rows)
-    return result_df
+    return result_df, len(rows)  # 결과 DataFrame과 검색된 결과의 개수를 반환
 
-
-
-
-# API 루트
-@app.route('/search', methods=['GET'])
+@application.route('/search', methods=['GET'])
 def search():
     keyword = request.args.get('keyword')
     if not keyword:
@@ -69,10 +61,10 @@ def search():
 
     df = load_data()
     if df is not None:
-        result_df = search_keyword_in_columns(df, keyword)
-        return jsonify(result_df.to_dict(orient='records'))
+        result_df, count = search_keyword_in_columns(df, keyword)  # 검색 결과와 개수를 받음
+        return jsonify({'results': result_df.to_dict(orient='records'), 'count': count})  # 결과와 개수를 함께 JSON 형태로 리턴
     else:
         return jsonify({'error': 'Data not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 80)))
+    application.run(host='0.0.0.0', port=5000)
